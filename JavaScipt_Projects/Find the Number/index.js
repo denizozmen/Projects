1ecdf5b49053279a16b4f12fedb0b615ca6e0ed3
denizozmen <mr.denizozmen@gmail.com>
2020-09-22 16:23:18 +0300

    update

--- JavaScipt_Projects/Find the Number/index.js
+++ JavaScipt_Projects/Find the Number/index.js
@@ -1,25 +1,52 @@
-// document.getElementById("random").innerHTML =
-// Math.floor(Math.random() * 100);
 
+let input = document.getElementById("text");
+let button = document.getElementById("btn");
+let statment = document.getElementById("statment");
+let tester = document.getElementById("tester");
 
+let rndm = Math.floor(Math.random()*101); 
+/* Rasgele sayı üretmek için Math (0 dahil 1   hariç)*/
 
-const random = Math.floor(Math.random() * 100)
+button.addEventListener("click",check);
 
+let counter = 0;
 
-
-document.getElementById("button").addEventListener("click", check)
-
-function check(){
-
-    const num = document.getElementById("text").value;
-    console.log(random);
-    // const random = Math.floor(Math.random() * 100)        
-
-    if (num == random) { return alert("Winner!")}
+function check() {
+   
+    let num = Number(input.value);
     
-    else if (num > random) {document.getElementById("statement").innerHTML = "I am thinking about higher number";}
+    if (isNaN(num) || num == "" ) {
 
-    else if (num < random ) {document.getElementById("statement").innerHTML = "I am thinking about lower number";}
+      statment.innerText = "Are you sure that is a number ?";
+      return;    
+    } 
 
+    if (rndm == num){
+        counter++;
+        alert(`You are the winner :smile:. You guest after ${counter} times!`);
+        
+        if (confirm("You wanna play again")){
+            window.location.reload();
+
+        }else{
+            window.close();
+        }
+
+        
+    }else if(rndm>num){
+        statment.innerText = "I am thinking abaout High Number";
+
+    }else {
+        statment.innerText ="I am thinking abaout Lower Number";
+    }
+
+    counter++;
+    tester.innerText=`This is your test number:${counter}`;
+
+    input.value ="";
+    input.focus();
+  
 
 }
+
+
